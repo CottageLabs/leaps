ec1d5a79019cc86853449396468d6e56419338dc
Mark MacGillivray <mark@cottagelabs.com>
2013-07-30 14:05:48 +0100

    setting so that logged in admin people can use the TEST school and university

--- portality/view/leaps/forms.py
+++ portality/view/leaps/forms.py
@@ -52,10 +52,11 @@ def student():
             "institutions": dropdowns('institution'),
             "advancedlevels": dropdowns('advancedlevel')
         }
-        if 'TEST' in selections['schools']:
-            selections['schools'] = [i for i in selections['schools'] if i != 'TEST']
-        if 'TEST' in selections['institutions']:
-            selections['institutions'] = [i for i in selections['institutions'] if i != 'TEST']
+        if not current_user.do_admin:
+            if 'TEST' in selections['schools']:
+                selections['schools'] = [i for i in selections['schools'] if i != 'TEST']
+            if 'TEST' in selections['institutions']:
+                selections['institutions'] = [i for i in selections['institutions'] if i != 'TEST']
         response = make_response(
             render_template(
                 'leaps/survey/survey.html', 
