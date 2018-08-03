abcef1e0bb17c4963f2e04dabf9c01a8bb513086
Mark MacGillivray <mark@cottagelabs.com>
2018-08-03 12:44:37 +0100

    change to force current signed in users to re-sign policy too

--- portality/view/leaps/admin.py
+++ portality/view/leaps/admin.py
@@ -10,6 +10,7 @@ from portality.core import app
 from portality.view.leaps.forms import dropdowns
 import portality.models as models
 
+from datetime import datetime
 
 
 blueprint = Blueprint('admin', __name__)
@@ -25,6 +26,11 @@ def restrict():
         abort(401)
     elif request.method == 'DELETE' and not current_user.do_admin:
         abort(401)
+    dp = current_user.data['last_updated'].split(' ')[0].split('-')
+    if datetime.date(int(dp[0],int(dp[1]),int(dp[2]))) < datetime.date(2018,8,1) and current_user.data.get('agreed_policy',False) == True:
+        current_user.data['previously_agreed_policy'] = True
+        current_user.data['agreed_policy'] = False
+        current_user.save()
     if not current_user.agreed_policy:
         return redirect('/account/policy?next=' + request.path)
     elif not current_user.view_admin:
