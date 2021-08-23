252c322c2b21e4769a1de2e098fb93e1eb1a50ae
Mark MacGillivray <mark@cottagelabs.com>
2021-08-23 05:30:57 +0100

    test bulk action plan email

--- portality/util.py
+++ portality/util.py
@@ -75,8 +75,6 @@ def generate_password(length=8):
 
 
 def send_mail(to, fro, subject, text, files=[], bcc=[]):
-    print('sending mail')
-    print(to)
     assert type(to)==list
     assert type(files)==list
     if bcc and not isinstance(bcc, list):
