e546736c337e75fd4529e7f7fa9e8bc895b096ee
Mark MacGillivray <mark@cottagelabs.com>
2022-08-29 23:05:24 +0100

    alter account settings

--- portality/view/leaps/account.py
+++ portality/view/leaps/account.py
@@ -58,7 +58,7 @@ def index():
             user = {'id':acc.id}
             if 'created_date' in acc.data:
                 user['created_date'] = acc.data['created_date']
-            if request.values.get('users',False) == False or (request.values['users'] == 'perform_and_manage_interviewers' and acc.data.get('perform_and_manage_interviewers','') != '') or (request.values['users'] == 'perform_interviews' and acc.data.get('perform_interviews','') != '') or (request.values['users'] == 'school' and acc.data.get('school','') != '') or (request.values['users'] == 'institution' and acc.data.get('institution','') != '') or (request.values['users'] == 'super' and acc.id in app.config['SUPER_USER']) or (request.values['users'] not in ['school','institution','super'] and acc.data.get(request.values['users'],False)):
+            if request.values.get('users',False) == False or (request.values['users'] == 'perform_and_manage_interviewers' and acc.data.get('perform_and_manage_interviewers',False) == True) or (request.values['users'] == 'perform_interviews' and acc.data.get('perform_interviews',False) == True) or (request.values['users'] == 'school' and acc.data.get('school','') != '') or (request.values['users'] == 'institution' and acc.data.get('institution','') != '') or (request.values['users'] == 'super' and acc.id in app.config['SUPER_USER']) or (request.values['users'] not in ['school','institution','super'] and acc.data.get(request.values['users'],False)):
                 users.append(user)
     if util.request_wants_json():
         resp = make_response( json.dumps(users, sort_keys=True, indent=4) )
