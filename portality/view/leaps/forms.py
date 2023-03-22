e3c51e117f286c9ea2b1ce1f02484d4476d7ab25
Mark MacGillivray <mark@cottagelabs.com>
2023-03-22 08:16:51 +0000

    fix forms when not logged in

--- portality/view/leaps/forms.py
+++ portality/view/leaps/forms.py
@@ -58,8 +58,8 @@ def student():
         for s in r['hits']['hits']:
             selections['school_categories'][re.sub('[^a-z]+', '', s['_source']['name'].lower())] = s['_source']['leaps_category']
 
-        if adminsettings.get('survey',False) or current_user.view_admin:
-            if current_user.is_anonymous() or not current_user.do_admin:
+        if adminsettings.get('survey',False) or not current_user.is_anonymous(): #current_user.view_admin:
+            if not current_user.do_admin: # or current_user.is_anonymous()
                 if 'TEST' in selections['schools']:
                     selections['schools'] = [i for i in selections['schools'] if i != 'TEST']
                 if 'TEST' in selections['institutions']:
