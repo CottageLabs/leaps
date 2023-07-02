9df2cf6ad56b021df1f0062e2142916a1065af2b
Mark MacGillivray <mark@cottagelabs.com>
2023-07-02 19:41:47 +0100

    2023 updates

--- portality/view/leaps/admin.py
+++ portality/view/leaps/admin.py
@@ -230,12 +230,15 @@ def studentassign():
         for i in s.get('hits',{}).get('hits',[]): 
             if len(selected) == 0 or i['_source']['id'] in selected:
                 student = models.Student.pull(i['_source']['id'])
-                if student.data.get('interviewer', False) != interviewer:
+                if student.data.get('interviewer', False) != interviewer and not student.data.get('interview', {}).get('emailed_date', False):
                     student.data['interviewer'] = interviewer
+                    student.data['status'] = 'allocated_to_interviewer'
                     student.save()
                     counter += 1
     flash(str(counter) + ' selected records were assigned to ' + interviewer, 'success')
-    return redirect('/admin/student')
+    rdu = '/admin/student'
+    if request.values.get('q',False): rdu += '?q=' + request.values.get('q')
+    return redirect(rdu)
 
 @blueprint.route('/student/<uuid>/assign/<iid>', methods=['POST'])
 def assignstudent(uuid=None,iid=None):
