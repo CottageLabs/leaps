6b3265feedec3ed94868dcc0203b124ca2771dbb
Mark MacGillivray <mark@cottagelabs.com>
2023-07-02 20:07:57 +0100

    fix admin query params

--- portality/view/leaps/admin.py
+++ portality/view/leaps/admin.py
@@ -237,7 +237,7 @@ def studentassign():
                     counter += 1
     flash(str(counter) + ' selected records were assigned to ' + interviewer, 'success')
     rdu = '/admin/student'
-    if request.values.get('q',False): rdu += '?' + urllib.urlencode({'source': json.dump(request.values.get('q'))})
+    if request.values.get('q',False): rdu += '?' + urllib.urlencode({'source': json.dumps(request.values.get('q')).replace('\\', '')})
     return redirect(rdu)
 
 @blueprint.route('/student/<uuid>/assign/<iid>', methods=['POST'])
